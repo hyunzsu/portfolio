a27a5f8a43ad78b9c3f8dc8ea15ad2f7a51e7ec5
hyunzsu <hyunzsu@naver.com>
2024-05-10 18:43:06 +0900

    [🗼markup] AboutSection 마크업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import MainSection from './_sections/MainSection';
+import AboutSection from "./_sections/AboutSection";
+import MainSection from "./_sections/MainSection";
 
 export default function Home() {
   return (
-    <main className=''>
+    <main>
       <MainSection />
+      <AboutSection />
     </main>
   );
 }
