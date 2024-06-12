5aebcc1ae903c6680f41ebc7d57e5a5cf41080fd
hyunzsu <hyunzsu@naver.com>
2024-06-12 15:52:48 +0900

    [🗼markup] Navbar 반응형 완성

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,26 +1,90 @@
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
 import styles from "./Navbar.module.css";
-import { TfiArrowTopRight, TfiGithub } from "react-icons/tfi";
+import { TfiArrowTopRight } from "react-icons/tfi";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header className={styles.header}>
-      <div>
-        <Link href="/">H.</Link>
-        <a href="/">
-          <span>Github</span>
-          <TfiArrowTopRight />
-        </a>
-        <a href="/">
-          <span>Resume</span>
-          <TfiArrowTopRight />
-        </a>
-        <nav>
-          <button>About,</button>
-          <button>Projects,</button>
-          <button>Contact</button>
-        </nav>
+      <div className={styles["header-container"]}>
+        <div className={styles["first-inner"]}>
+          <Link href="/" className={styles.logo}>
+            H.
+          </Link>
+          <div className={styles["link-container"]}>
+            <a
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.link}
+            >
+              <span>Github</span>
+              <TfiArrowTopRight />
+            </a>
+            <a
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.link}
+            >
+              <span>Resume</span>
+              <TfiArrowTopRight />
+            </a>
+          </div>
+        </div>
+        <ul className={`${styles.nav} ${menuOpen ? styles.show : ""}`}>
+          <li className={styles["link-item"]}>
+            <a
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.link}
+            >
+              <span>Github</span>
+              <TfiArrowTopRight />
+            </a>
+          </li>
+          <li className={styles["link-item"]}>
+            <a
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.link}
+            >
+              <span>Resume</span>
+              <TfiArrowTopRight />
+            </a>
+          </li>
+          <li>
+            <button className={styles.button}>About</button>
+          </li>
+          <li>
+            <button className={styles.button}>Projects</button>
+          </li>
+          <li>
+            <button className={styles.button}>Contact</button>
+          </li>
+        </ul>
+        <div
+          className={`${styles["hamburger-menu"]} ${menuOpen ? styles.open : ""}`}
+          onClick={toggleMenu}
+        >
+          <div className={styles["hamburger-icon"]}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
       </div>
+      {menuOpen && <div className={styles.overlay} onClick={toggleMenu}></div>}
     </header>
   );
 }
