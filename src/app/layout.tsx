35ed0b2a8fd132fb5e697cd49bdfcaf3f9262a10
hyunzsu <hyunzsu@naver.com>
2024-06-15 13:06:32 +0900

    [🗼markup] main 섹션 반응형 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ export const metadata: Metadata = {
   title: "hyunzsu portfolio",
   description: "프론트엔드 개발자 현지수 포트폴리오",
   keywords: ["포트폴리오", "넥스트", "portfolio", "next.js"],
+  icons: {
+    icon: "/about-photo.svg",
+  },
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import About from "./about/page";
 import Main from "./main/page";
 
 export default function Home() {
   return (
-    <section className="">
+    <section className="container">
       <Main />
-      <About />
     </section>
   );
 }
