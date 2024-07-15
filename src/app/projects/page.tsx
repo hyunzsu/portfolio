ebcc34f8ffa09ec7d086a7dde9f11eb3c14609c5
hyunzsu <hyunzsu@naver.com>
2024-07-15 15:11:44 +0900

    [🔰type] project 타입 수정

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,7 @@
 import ProjectList from "@/components/Project/ProjectList/ProjectList";
 import { getProjectData } from "../api/supabaseClient";
 import styles from "./page.module.css";
+import { Project } from "@/types/project";
 
 export default async function Projects() {
   const projects = await getProjectData();
@@ -13,7 +14,7 @@ export default async function Projects() {
         프로젝트를 클릭하면 해당 프로젝트의 상세 페이지로 이동합니다.
       </span>
 
-      <ProjectList projects={projects} />
+      <ProjectList projects={projects as Project[]} />
     </div>
   );
 }
