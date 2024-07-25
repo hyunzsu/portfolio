1ada29fe73746ad3ce8013d3a0cd216c79f718fc
hyunzsu <hyunzsu@naver.com>
2024-07-25 20:36:23 +0900

    [💄style] project 섹션 ProjectCard 수정

--- src/components/Project/ProjectCard/ProjectCard.tsx
+++ src/components/Project/ProjectCard/ProjectCard.tsx
@@ -11,33 +11,33 @@ interface ProjectCardProps {
 export default function ProjectCard({ project }: ProjectCardProps) {
   return (
     <article className={styles.article}>
-      <div className={styles["image-container"]}>
-        <Image
-          className={styles.image}
-          src={project.main_images[0]}
-          alt={`${project.title} 이미지`}
-          fill
-          sizes="100vw"
-          priority
-        />
-      </div>
-      <div className={styles.content}>
-        <div className={styles.first}>
-          <h3 className={styles.title}>{project.title}</h3>
-          <ul>
-            {project.category.map((tag, index) => (
-              <li key={index}>
-                <TypeTag type={tag} />
-              </li>
-            ))}
-          </ul>
+      <p className={styles.inner}>
+        <div className={styles["image-container"]}>
+          <Image
+            className={styles.image}
+            src={project.main_images[0]}
+            alt={`${project.title} 이미지`}
+            fill
+            sizes="100vw"
+            priority
+          />
         </div>
-        <p className={styles.period}>
-          {project.start_date} ~ {project.end_date}
-        </p>
-        <p className={styles.intro}>{project.intro}</p>
-      </div>
-      <div className={styles.second}>
+        <div className={styles.content}>
+          <div className={styles.first}>
+            <h3 className={styles.title}>{project.title}</h3>
+            <ul>
+              {project.category.map((tag, index) => (
+                <li key={index}>
+                  <TypeTag type={tag} />
+                </li>
+              ))}
+            </ul>
+          </div>
+          <p className={styles.period}>
+            {project.start_date} ~ {project.end_date}
+          </p>
+          <p className={styles.intro}>{project.intro}</p>
+        </div>
         <ul className={styles.stack}>
           {project.stacks.map((stack, index) => (
             <li key={index}>
@@ -45,48 +45,42 @@ export default function ProjectCard({ project }: ProjectCardProps) {
             </li>
           ))}
         </ul>
-        {/* <div className={styles.set}>
-          <div>
-            {project.github_url && (
-              <a
-                target="_blank"
-                href={project.github_url}
-                rel="noopener noreferrer"
-              >
-                <Image
-                  src="/icons/github.svg"
-                  alt="깃허브 아이콘"
-                  priority
-                  quality={100}
-                  width={0}
-                  height={0}
-                  sizes="100vw"
-                  style={{ width: "auto", height: "auto" }}
-                />
-              </a>
-            )}
-          </div>
-          <div>
-            {project.service_url && (
-              <a
-                target="_blank"
-                href={project.service_url}
-                rel="noopener noreferrer"
-              >
-                <Image
-                  src="/icons/deploy.svg"
-                  alt="서비스 아이콘"
-                  priority
-                  quality={100}
-                  width={0}
-                  height={0}
-                  sizes="100vw"
-                  style={{ width: "auto", height: "auto" }}
-                />
-              </a>
-            )}
-          </div>
-        </div> */}
+      </p>
+      <div className={styles.set}>
+        <div className={styles["img-content"]}>
+          {project.github_url && (
+            <a
+              target="_blank"
+              href={project.github_url}
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/icons/github.svg"
+                alt="깃허브 아이콘"
+                priority
+                width={300}
+                height={40}
+              />
+            </a>
+          )}
+        </div>
+        <div className={styles["img-content"]}>
+          {project.service_url && (
+            <a
+              target="_blank"
+              href={project.service_url}
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/icons/deploy.svg"
+                alt="서비스 아이콘"
+                priority
+                width={300}
+                height={40}
+              />
+            </a>
+          )}
+        </div>
       </div>
     </article>
   );
